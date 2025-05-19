fee6fd1754fae980242ff8aefa0ca75c3ca617f9
danicnb <danieltondopaez@gmail.com>
2025-05-19 21:47:32 +0200

    Finish PR2

--- src/main/java/edu/uoc/epcsd/notification/domain/service/NotificationServiceImpl.java
+++ src/main/java/edu/uoc/epcsd/notification/domain/service/NotificationServiceImpl.java
@@ -20,11 +20,39 @@ public class NotificationServiceImpl implements NotificationService {
     @Value("${productService.getProductDetails.url}")
     private String productServiceUrl;
 
+    /**
+     * Notifies users that a product item is now available by retrieving their alerts from the User microservice.
+     *
+     * @param productMessage contains the product ID of the product that has become available
+     * @throws RuntimeException logged as an error if the HTTP request to the User microservice fails
+     *
+     * This method builds a GET request to the User microservice to obtain the list of users
+     * who have alerts for the specified product and current date. If users are returned, it simulates
+     * sending an email notification by logging the action with INFO level for each user.
+     */
     @Override
     public void notifyProductAvailable(ProductMessage productMessage) {
+        log.info("notifyProductAvailable - productId={}, date={}", productMessage.getProductId(), LocalDate.now());
 
-            // TODO: Use RestTemplate with the above userServiceUrl to query the User microservice in order to get the users that have an alert for the specified product (the date specified in the parameter may be the actual date: LocalDate.now()).
-            //  Then simulate the email notification for the alerted users by logging a line with INFO level for each user saying "Sending an email to user " + the user fullName
+        String url = userServiceUrl
+                .replace("{productId}", String.valueOf(productMessage.getProductId()))
+                .replace("{availableOnDate}", LocalDate.now().toString()); // format ISO
 
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+
+            GetUserResponse[] users = restTemplate.getForObject(url, GetUserResponse[].class);
+
+            if (users != null && users.length > 0) {
+                for (GetUserResponse user : users) {
+                    log.info("Sending an email to user {}", user.getFullName());
+                }
+            } else {
+                log.info("No users to notify for product {}", productMessage.getProductId());
+            }
+
+        } catch (Exception e) {
+            log.error("Error notifying users for product {}", productMessage.getProductId(), e);
+        }
     }
 }
